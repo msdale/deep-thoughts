839f13d6dd5a8bc8086453168fa2245b4bf421cc
Mark Dale <msdaledad@gmail.com>
2022-03-09 18:45:11 -0500

    starting 2.3.1

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,11 +1,11 @@
 const mongoose = require('mongoose');
 
 mongoose.connect(
-  process.env.MONGODB_URI || 'mongodb://localhost:27017/deep-thoughts',
-  {
+  process.env.MONGODB_URI || 'mongodb://localhost:27017/deep-thoughts'
+  /*{
     useNewUrlParser: true, 
     useUnifiedTopology: true,
-  }
+  }*/
 );
 
 module.exports = mongoose.connection;
