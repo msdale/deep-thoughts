625d38fb1429147d275abd3575ce3acb24d02192
Mark Dale <msdaledad@gmail.com>
2022-03-12 15:30:20 -0500

    updated to 21.4.zip

--- client/src/App.js
+++ client/src/App.js
@@ -1,21 +1,21 @@
 import React from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import {
   ApolloClient,
   InMemoryCache,
   ApolloProvider,
   createHttpLink,
 } from '@apollo/client';
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 
 import Header from './components/Header';
 import Footer from './components/Footer';
+
+import Home from './pages/Home';
 import Login from './pages/Login';
 import NoMatch from './pages/NoMatch';
 import SingleThought from './pages/SingleThought';
 import Profile from './pages/Profile';
 import Signup from './pages/Signup';
-
-import Home from './pages/Home';
 
 const httpLink = createHttpLink({
   uri: '/graphql',
@@ -39,6 +39,7 @@ function App() {
               <Route exact path="/signup" component={Signup} />
               <Route exact path="/profile/:username?" component={Profile} />
               <Route exact path="/thought/:id" component={SingleThought} />
+
               <Route component={NoMatch} />
             </Switch>
           </div>
@@ -48,4 +49,5 @@ function App() {
     </ApolloProvider>
   );
 }
+
 export default App;
--- client/src/pages/Profile.js
+++ client/src/pages/Profile.js
@@ -2,17 +2,16 @@ import React from 'react';
 import { useParams } from 'react-router-dom';
 
 import ThoughtList from '../components/ThoughtList';
+import FriendList from '../components/FriendList';
 
 import { useQuery } from '@apollo/client';
 import { QUERY_USER } from '../utils/queries';
 
-import FriendList from '../components/FriendList';
-
-const Profile = () => {
+const Profile = (props) => {
   const { username: userParam } = useParams();
 
   const { loading, data } = useQuery(QUERY_USER, {
-    variables: { username: userParam }
+    variables: { username: userParam },
   });
 
   const user = data?.user || {};
@@ -31,7 +30,10 @@ const Profile = () => {
 
       <div className="flex-row justify-space-between mb-3">
         <div className="col-12 mb-3 col-lg-8">
-          <ThoughtList thoughts={user.thoughts} title={`${user.username}'s thoughts...`} />
+          <ThoughtList
+            thoughts={user.thoughts}
+            title={`${user.username}'s thoughts...`}
+          />
         </div>
 
         <div className="col-12 col-lg-3 mb-3">
@@ -45,4 +47,5 @@ const Profile = () => {
     </div>
   );
 };
+
 export default Profile;
